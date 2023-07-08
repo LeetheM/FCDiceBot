b971eace1ae5cd412d30dddb703191a9d8e6705f
LeetheM <35052653+LeetheM@users.noreply.github.com>
2023-07-08 00:06:18 -0700

    1.35 fix A
    adding missing files

---
+++ FChatDicebot/FChatDicebot/BotCommands/GenerateChipsCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FChatDicebot.BotCommands.Base;
+using FChatDicebot.SavedData;
+using Newtonsoft.Json;
+
+namespace FChatDicebot.BotCommands
+{
+    public class GenerateChipsCode : ChatBotCommand
+    {
+        public GenerateChipsCode()
+        {
+            Name = "generatechipscode";
+            RequireBotAdmin = true;
+            RequireChannelAdmin = false;
+            RequireChannel = false;
+            LockCategory = CommandLockCategory.ChannelScores;
+        }
+
+        public override void Run(BotMain bot, BotCommandController commandController, string[] rawTerms, string[] terms, string characterName, string channel, UserGeneratedCommand command)
+        {
+        }
+    }
+}
